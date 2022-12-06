cc01349bdda2e0dbfc85bf7b3bb9ea657a6ad184
Aria Taylor <ari@aricodes.net>
2022-12-06 14:51:02 -0500

    Initial commit

---
+++ lib/snek.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "snek/version"
+
+module Snek
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/snek_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Snek do
+  it "has a version number" do
+    expect(Snek::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
